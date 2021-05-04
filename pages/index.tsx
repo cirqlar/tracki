1ea418ff3f038b50cbf69f877891aef154068239
Ibitoye Ayanfeoluwa <ibitoyeayanfeoluwa@gmail.com>
2021-05-04 16:21:11 +0100

    chore: fix index tests

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,8 +2,8 @@ export default function Home() {
   return (
     <div className="w-full h-full flex flex-col items-center dark:bg-black text-black dark:text-white">
       <main className="flex-auto flex flex-col items-center justify-center">
-        <img className="dark:hidden w-[100px]" src="/logo/white-trans.svg" alt="tracki" />
-        <img className="hidden dark:block w-[100px]" src="/logo/black-trans.svg" alt="tracki" />
+        <img className="dark:hidden w-[100px]" src="/logo/white-trans.svg" alt="black tracki logo" />
+        <img className="hidden dark:block w-[100px]" src="/logo/black-trans.svg" alt="white tracki logo" />
         <h1 className="text-4xl">tracki</h1>
         <a className="bg-green-500 rounded uppercase text-white py-2 px-4 text-base mt-5">Begin</a>
         <a className="underline text-xs mt-4 font-light">Already have an account?</a>
