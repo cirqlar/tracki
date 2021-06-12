bff791c348518fdc2a5dbaecaa9d613497eaacdb
Ibitoye Ayanfeoluwa <ibitoyeayanfeoluwa@gmail.com>
2021-06-12 17:16:12 +0100

    feat: use env url in head

--- components/layout/head.tsx
+++ components/layout/head.tsx
@@ -1,5 +1,10 @@
 import Head from "next/head";
 
+const PUBLIC_URL = process.env.NEXT_PUBLIC_VERCEL_URL ?? "localhost:3000";
+const APP_URL = `https://${PUBLIC_URL}`
+const APP_NAME = "tracki";
+const APP_DESCRIPTION = "Tracki is a web app that helps you track whatever you want.";
+
 export default function HeadTags() {
   return (
     <Head>
@@ -15,16 +20,16 @@ export default function HeadTags() {
         name="viewport"
         content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no"
       />
-      <meta name="description" content="Tracki is a web app that helps you track whatever you want." />
+      <meta name="description" content={APP_DESCRIPTION} />
       <meta name="keywords" content="tracker" />
-      <title>tracki</title>
+      <title>{APP_NAME}</title>
 
       {/* <!-- Android  --> */}
       <meta name="theme-color" content="red" />
       <meta name="mobile-web-app-capable" content="#eb5757" />
 
       {/* <!-- iOS --> */}
-      <meta name="apple-mobile-web-app-title" content="tracki" />
+      <meta name="apple-mobile-web-app-title" content={APP_NAME} />
       <meta name="apple-mobile-web-app-capable" content="yes" />
       <meta name="apple-mobile-web-app-status-bar-style" content="default" />
 
@@ -32,7 +37,6 @@ export default function HeadTags() {
       <link href="/icon/favicon-16x16-42605.png" rel="icon" type="image/png" sizes="16x16" />
       <link href="/icon/favicon-32x32-42605.png" rel="icon" type="image/png" sizes="32x32" />
       <link href="/icon/favicon-48x48-42605.png" rel="icon" type="image/png" sizes="48x48" />
-      
 
       {/* <!-- iOS  --> */}
       <link href="/icon/apple-57x57-42605.png" rel="apple-touch-icon" sizes="57x57" />
@@ -45,7 +49,6 @@ export default function HeadTags() {
       <link href="/icon/apple-152x152-42605.png" rel="apple-touch-icon" sizes="152x152" />
       <link href="/icon/apple-180x180-42605.png" rel="apple-touch-icon" sizes="180x180" />
 
-
       {/* <!-- Android  --> */}
       <link href="/icon/android-192x192-42605.png" rel="icon" sizes="192x192" />
 
@@ -54,17 +57,19 @@ export default function HeadTags() {
 
       {/* <!-- Open Graph / Facebook --> */}
       <meta property="og:type" content="website" />
-      {/* <meta property="og:url" content="https://ishraing.org/" /> */}
-      <meta property="og:title" content="tracki" />
-      <meta property="og:description" content="Tracki is a web app that helps you track whatever you want." />
-      {/* <meta property="og:image" content="https://ishraing.org/logo/ishrai-white.jpg" /> */}
+      <meta property="og:title" content={APP_NAME} />
+      <meta property="og:description" content={APP_DESCRIPTION} />
+      <meta property="og:site_name" content={APP_NAME} />
+      <meta property="og:url" content={APP_URL} />
+      <meta property="og:image" content={`${APP_URL}/public/icon/android-192x192-42605.png`} />
 
       {/* <!-- Twitter --> */}
-      <meta property="twitter:card" content="summary_large_image" />
-      {/* <meta property="twitter:url" content="https://ishraing.org/" /> */}
-      <meta property="twitter:title" content="tracki" />
-      <meta property="twitter:description" content="Tracki is a web app that helps you track whatever you want." />
-      {/* <meta property="twitter:image" content="https://ishraing.org/logo/ishrai-white.jpg" /> */}
+      <meta name='twitter:card' content='summary_large_image' />
+      <meta name='twitter:url' content={APP_URL} />
+      <meta name='twitter:title' content={APP_NAME} />
+      <meta name='twitter:description' content={APP_DESCRIPTION} />
+      <meta name='twitter:image' content={`${APP_URL}/public/icon/android-192x192-42605.png`} />
+      <meta name='twitter:creator' content='@uiAyanfe' />
     </Head>
   );
 }
