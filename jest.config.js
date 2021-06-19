256c828bba176d33d83df8c756e49f2556d461d5
Ibitoye Ayanfeoluwa <ibitoyeayanfeoluwa@gmail.com>
2021-06-19 15:51:37 +0100

    feat: remove ts-jest and write first test
    also add pages alias

--- jest.config.js
+++ jest.config.js
@@ -1,16 +1,16 @@
 module.exports = {
-  preset: "ts-jest",
-  testEnvironment: "node",
+  testEnvironment: "jsdom",
   setupFilesAfterEnv: ["./jest.setup.ts"],
   collectCoverage: true,
   collectCoverageFrom: [
     "./components/**/*.{js,jsx,ts,tsx}",
     "./pages/**/*.{js,jsx,ts,tsx}",
+    "!./pages/_app.tsx",
     "./lib/**/*.{js,jsx,ts,tsx}",
   ],
-  globals: {
-    "ts-jest": {
-      babelConfig: true,
-    },
+  moduleNameMapper: {
+    "^@/pages(.*)$": "<rootDir>/pages$1",
+    "^@/components(.*)$": "<rootDir>/components$1",
+    "^@/lib(.*)$": "<rootDir>/lib$1",
   },
 };
