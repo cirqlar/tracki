d2580cc03610a1c5c37c15c93483292f3a28854e
Ayanfe Ibitoye <ibitoyeayanfeoluwa@gmail.com>
2025-01-26 14:43:48 +0100

    feat: config without tauri option, add tailwind and prettier

--- vite.config.ts
+++ vite.config.ts
@@ -1,32 +1,46 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import tailwindcss from '@tailwindcss/vite';
 
 // @ts-expect-error process is a nodejs global
 const host = process.env.TAURI_DEV_HOST;
 
-// https://vitejs.dev/config/
-export default defineConfig(async () => ({
-  plugins: [react()],
+// @ts-expect-error process is a nodejs global
+const WITHOUT_TAURI = !!(process.env.WITHOUT_TAURI ?? false);
 
-  // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
-  //
-  // 1. prevent vite from obscuring rust errors
-  clearScreen: false,
-  // 2. tauri expects a fixed port, fail if that port is not available
-  server: {
-    port: 1420,
-    strictPort: true,
-    host: host || false,
-    hmr: host
-      ? {
-          protocol: "ws",
-          host,
-          port: 1421,
-        }
-      : undefined,
-    watch: {
-      // 3. tell vite to ignore watching `src-tauri`
-      ignored: ["**/src-tauri/**"],
-    },
-  },
-}));
+// https://vitejs.dev/config/
+export default defineConfig(async () => {
+	const plugins = [react(), tailwindcss()];
+
+	if (WITHOUT_TAURI) {
+		return {
+			plugins,
+		};
+	} else {
+		return {
+			plugins,
+
+			// Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
+			//
+			// 1. prevent vite from obscuring rust errors
+			clearScreen: false,
+			// 2. tauri expects a fixed port, fail if that port is not available
+			server: {
+				port: 1420,
+				strictPort: true,
+				host: host || false,
+				hmr: host
+					? {
+						protocol: "ws",
+						host,
+						port: 1421,
+					}
+					: undefined,
+				watch: {
+					// 3. tell vite to ignore watching `src-tauri`
+					ignored: ["**/src-tauri/**"],
+				},
+			},
+		};
+	}
+});
