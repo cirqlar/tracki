804b26ac647081af80dccecff79036be9b28f528
Ayanfe Ibitoye <ibitoyeayanfeoluwa@gmail.com>
2025-01-27 19:27:00 +0100

    feat: add eslint

---
+++ eslint.config.js
@@ -0,0 +1,21 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+import pluginReact from "eslint-plugin-react";
+import pluginReactHooks from "eslint-plugin-react-hooks";
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+	{ files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"] },
+	{ languageOptions: { globals: { ...globals.browser, ...globals.node } } },
+	pluginJs.configs.recommended,
+	...tseslint.configs.recommended,
+	pluginReact.configs.flat["jsx-runtime"],
+	{
+		plugins: {
+			"react-hooks": pluginReactHooks,
+		},
+		rules: pluginReactHooks.configs.recommended.rules,
+	},
+	{ ignores: ["src-tauri", "dist/"] },
+];
