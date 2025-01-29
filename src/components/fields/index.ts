a39a4ed0c689527d46e3025561eda7ecbbfa86b7
Ayanfe Ibitoye <ibitoyeayanfeoluwa@gmail.com>
2025-01-29 20:28:53 +0100

    fix: Turns out ReactNode covers promises

--- src/components/fields/index.ts
+++ src/components/fields/index.ts
@@ -1,4 +1,3 @@
-import { LazyExoticComponent } from "react";
 import dateField from "./dates";
 
 interface FieldNewThingProps<T> {
@@ -6,39 +5,29 @@ interface FieldNewThingProps<T> {
 	fieldSettings: T;
 	updateFieldData: (fieldData: T) => unknown;
 }
-type NewThingComponent<T> = (props: FieldNewThingProps<T>) => React.ReactNode;
 
 interface FieldAddMenuProps {
 	variant: string;
 }
-type AddMenuIcon = (props: FieldAddMenuProps) => React.ReactNode;
 
 interface FieldAddEntryProps {
 	schema: string;
 }
-type AddEntryComponent = (props: FieldAddEntryProps) => React.ReactNode;
 
 interface FieldDisplayEntryProps {
 	schema: string;
 	info: string;
 }
-type DisplayEntryComponent = (props: FieldDisplayEntryProps) => React.ReactNode;
 
 export interface Field<T> {
 	id: string;
 	friendlyName: (variant: string) => string;
 	variants: string | string[];
 	fieldSettingsToSchemaString: (fieldSettings: T) => string;
-	NewThingComponent:
-		| NewThingComponent<T>
-		| LazyExoticComponent<NewThingComponent<T>>;
-	AddMenuIcon: AddMenuIcon | LazyExoticComponent<AddMenuIcon>;
-	AddEntryComponent:
-		| AddEntryComponent
-		| LazyExoticComponent<AddEntryComponent>;
-	DisplayEntryComponent:
-		| DisplayEntryComponent
-		| LazyExoticComponent<DisplayEntryComponent>;
+	NewThingComponent: (props: FieldNewThingProps<T>) => React.ReactNode;
+	AddMenuIcon: (props: FieldAddMenuProps) => React.ReactNode;
+	AddEntryComponent: (props: FieldAddEntryProps) => React.ReactNode;
+	DisplayEntryComponent: (props: FieldDisplayEntryProps) => React.ReactNode;
 }
 
 export const FIELDS: Field<unknown>[] = [dateField];
